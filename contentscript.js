f175e2336c452e9119099e98b6763b564052621a
AlvinHKH <email@alvinhkh.com>
2014-07-09 13:40:39 +0800

    fix missing button class

--- contentscript.js
+++ contentscript.js
@@ -176,11 +176,13 @@ ytrp = {
 		ytrp.link_next = document.getElementsByClassName('next-playlist-list-item')[0];
 		if (document.getElementsByClassName('playlist-nav-controls').length > 0)
 		ytrp.button_container_class = document.getElementsByClassName('playlist-nav-controls')[0];
-		if (document.getElementsByClassName('toggle-menu-visibility').length > 0)
-		ytrp.button_class = document.getElementsByClassName('toggle-menu-visibility')[0].className.replace(/( )?toggle-menu-visibility/, '');
+		if (document.getElementsByClassName('toggle-autoplay').length > 0)
+		ytrp.button_class = document.getElementsByClassName('toggle-autoplay')[0].className.replace(/( )?toggle-autoplay/, '');
+		else if (document.getElementsByClassName('next-playlist-list-item').length > 0)
+		ytrp.button_class = document.getElementsByClassName('next-playlist-list-item')[0].className.replace(/( )?next-playlist-list-item/, '');
 		if (document.getElementsByClassName('shuffle-playlist').length > 0)
 		ytrp.button_shuffle = document.getElementsByClassName('shuffle-playlist')[0];
-
+		
 		sessionStorage['yt-playlist-id'] = ytrp.getPlaylistId();
 
 		try {
